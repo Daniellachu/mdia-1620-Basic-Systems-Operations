e6519e1299c3e0730947293277225947a368a2f7
daniella Dela Cruz <ddelacruz17@my.bcit.ca>
2022-09-16 12:15:49 -0700

    adding more code

---
+++ week2/test.js
@@ -0,0 +1,19 @@
+//DAta Type of object 
+
+/**
+ * this
+ * is
+ * a
+ * multi
+ * line
+ * comment
+ */
+
+// to create an object
+var grannySmith = {
+    color:"green",
+    originLocation:"Australia",
+    originDate:1868
+};
+
+console.log(grannySmith);
